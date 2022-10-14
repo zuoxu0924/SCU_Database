79e0e9facdfc94e1ea0dfdccd49fe1f2f9a9a759
Ye Xianglun <91407103+zuoxu0924@users.noreply.github.com>
2022-10-14 22:24:22 +0800

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yneversky/ynwork/SCUDB/scudb_initial
-# Build directory: /home/yneversky/ynwork/SCUDB/scudb_initial/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
