4db282cc256ca3b3d6f03413e0fcf1d3d2b59000
Yeh Hsianglun <91407103+zuoxu0924@users.noreply.github.com>
2022-11-12 16:36:56 +0800

    Update extendible_hash.h
    Add variables: struct Bucket, bucket_size, global_depth, bucket_num, dict, mutex - mtx;
    Add helper function: split() to split buckets.

--- project1/src/include/hash/extendible_hash.h
+++ project1/src/include/hash/extendible_hash.h
@@ -12,6 +12,10 @@
 #include <cstdlib>
 #include <vector>
 #include <string>
+#include <map>
+#include <mutex>
+#include <memory>
+#include <cmath>
 
 #include "hash/hash_table.h"
 
@@ -19,12 +23,15 @@ namespace scudb {
 
 template <typename K, typename V>
 class ExtendibleHash : public HashTable<K, V> {
+
 public:
   // constructor
-  ExtendibleHash(size_t size);
+  ExtendibleHash(size_t size=64);
+  ExtendibleHash();
   // helper function to generate hash addressing
   size_t HashKey(const K &key);
   // helper function to get global & local depth
+  //local depth <= global depth
   int GetGlobalDepth() const;
   int GetLocalDepth(int bucket_id) const;
   int GetNumBuckets() const;
@@ -32,8 +39,22 @@ public:
   bool Find(const K &key, V &value) override;
   bool Remove(const K &key) override;
   void Insert(const K &key, const V &value) override;
+  // size_t getId(const K &key) const;
 
 private:
-  // add your own member variables here
+  struct Bucket{
+        Bucket(size_t id, int localdepth) : id_t(id), local_depth(localdepth) {}
+        size_t id_t = 0;
+        int local_depth = 0;
+        std::map<K, V> records;
+        std::mutex mtx;
+  };
+  size_t bucket_size; //size of bucket
+  int global_depth; 
+  int bucket_num; //the number of buckets
+  std::vector<std::shared_ptr<Bucket>> dict;  //dictionary to store the address of bucket
+  mutable std::mutex mtx; //mutex lock thread security
+  std::shared_ptr<Bucket> split(std::shared_ptr<Bucket> &bucket);  //split the bucket
+
 };
 } // namespace scudb
